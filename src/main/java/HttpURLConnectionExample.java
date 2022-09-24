2525bbd8db188c7a8e79a24a5263e900a1ee45b6
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-09-24 18:02:34 +0530

    updated

--- src/main/java/HttpURLConnectionExample.java
+++ src/main/java/HttpURLConnectionExample.java
@@ -37,28 +37,21 @@ public class HttpURLConnectionExample {
 
         URL url = new URL("https://dummy.restapiexample.com/api/v1/create");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
         connection.setRequestMethod("POST");
-
         /// in postman --> post request header section --> they is key, value --> so our send request context is --> content-type, application/json like (key, values pair)
         connection.setRequestProperty("Content-Type","application/json");
         // we want to send out the output to the url so we have to set as TRUE, by default it will in FALSE
         connection.setDoOutput(true);
-
         // escaping the string is " \"name\" "
         String json = "{\"name\":\"Balai\",\"salary\":\"55000\",\"age\":\"23\"}";
 
         byte[] inputjson = json.getBytes();
-
         OutputStream outputStream = connection.getOutputStream();
         outputStream.write(inputjson);
-
         System.out.println("Response code"+connection.getResponseCode());
         System.out.println("Message "+connection.getResponseMessage());
-
         InputStream inputStream = connection.getInputStream();
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         String lines;
         StringBuffer buffer = new StringBuffer();
@@ -68,13 +61,9 @@ public class HttpURLConnectionExample {
         }
         System.out.println(buffer);
     }
-
     public static void main(String[] args) throws IOException {
-
         HttpURLConnectionExample connectionExample = new HttpURLConnectionExample();
         //connectionExample.GetMethod();
         connectionExample.PostMethod();
-
-
     }
 }
