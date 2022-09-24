de7d5d73e3895aa55a2585223672155343c0a40c
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-09-24 16:35:30 +0530

    updated

--- src/main/java/HttpURLConnectionExample.java
+++ src/main/java/HttpURLConnectionExample.java
@@ -19,4 +19,12 @@ public class HttpURLConnectionExample {
 
     }
 
+    public static void main(String[] args) throws IOException {
+
+        HttpURLConnectionExample connectionExample = new HttpURLConnectionExample();
+        connectionExample.GetMethod();
+
+
+
+    }
 }
