4a01b3a8ebc008031c998caa55bac2eec614bd0e
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-09-24 16:27:37 +0530

    updated

--- src/main/java/HttpURLConnectionExample.java
+++ src/main/java/HttpURLConnectionExample.java
@@ -1,12 +1,21 @@
-import java.net.URI;
+import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 
 public class HttpURLConnectionExample {
 
-    public void GetMethod(){
+    public void GetMethod() throws IOException {
 
-        URI uri
+        URL uri = new URL("https://dummy.restapiexample.com/api/v1/employees");
+        HttpURLConnection connection = (HttpURLConnection) uri.openConnection();
+        connection.setRequestMethod("Get");
+        connection.connect();
 
-
+        int statusCode = connection.getResponseCode();
+        System.out.println("Status code is "+statusCode);
 
     }
 
