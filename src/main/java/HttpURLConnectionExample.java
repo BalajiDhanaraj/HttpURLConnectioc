99087acbd0880a5584a2989544b270cd82ca032a
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-09-24 18:11:21 +0530

    updated

--- src/main/java/HttpURLConnectionExample.java
+++ src/main/java/HttpURLConnectionExample.java
@@ -61,9 +61,43 @@ public class HttpURLConnectionExample {
         }
         System.out.println(buffer);
     }
+
+    public void PutMethod() throws IOException {
+        //{"status":"success","data":{"name":"Balai","salary":"55000","age":"23","id":896},"message":"Successfully! Record has been added."}
+
+        URL url = new URL("https://dummy.restapiexample.com/api/v1/update/896");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("PUT");
+        /// in postman --> post request header section --> they is key, value --> so our send request context is --> content-type, application/json like (key, values pair)
+        connection.setRequestProperty("Content-Type","application/json");
+        // we want to send out the output to the url so we have to set as TRUE, by default it will in FALSE
+        connection.setDoOutput(true);
+        // escaping the string is " \"name\" "
+        String json = "{\"name\":\"Balai\",\"salary\":\"55000\",\"age\":\"24\"}";
+
+        byte[] inputjson = json.getBytes();
+        OutputStream outputStream = connection.getOutputStream();
+        outputStream.write(inputjson);
+        System.out.println("Response code"+connection.getResponseCode());
+        System.out.println("Message "+connection.getResponseMessage());
+        InputStream inputStream = connection.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String lines;
+        StringBuffer buffer = new StringBuffer();
+
+        while((lines = bufferedReader.readLine())!=null){
+            buffer.append(lines);
+        }
+        System.out.println(buffer);
+
+
+
+    }
     public static void main(String[] args) throws IOException {
         HttpURLConnectionExample connectionExample = new HttpURLConnectionExample();
         //connectionExample.GetMethod();
-        connectionExample.PostMethod();
+        //connectionExample.PostMethod();
+        connectionExample.PutMethod();
     }
 }
