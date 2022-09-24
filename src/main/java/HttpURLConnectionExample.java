4dd289dcd4c7d809c2601c380e92bc919a9791d5
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-09-24 16:51:43 +0530

    updated

--- src/main/java/HttpURLConnectionExample.java
+++ src/main/java/HttpURLConnectionExample.java
@@ -1,21 +1,36 @@
+import java.io.*;
 import java.net.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 
 public class HttpURLConnectionExample {
 
     public void GetMethod() throws IOException {
 
-        URL uri = new URL("https://dummy.restapiexample.com/api/v1/employees");
-        HttpURLConnection connection = (HttpURLConnection) uri.openConnection();
+        URL url = new URL("https://dummy.restapiexample.com/api/v1/employees");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         connection.connect();
 
         int statusCode = connection.getResponseCode();
         System.out.println("Status code is "+statusCode);
+
+        String message = connection.getResponseMessage();
+        System.out.println("Message "+message);
+
+        // below line is used to read the JSON body
+
+        InputStream inputStream = connection.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String lines ;
+        StringBuffer buffer = new StringBuffer();
+
+        while ((lines=bufferedReader.readLine())!=null){
+            buffer.append(lines);
+        }
+        System.out.println(buffer);
+
 
     }
 
